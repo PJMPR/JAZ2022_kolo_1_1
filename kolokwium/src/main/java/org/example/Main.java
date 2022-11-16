268cac564216d7547c8779846a6b49ff28663906
adam urbanowicz <adam.urbanow@gmail.com>
2022-11-16 14:32:20 +0100

    k1

---
+++ kolokwium/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Zaczynamy. Życzę powodzenia :)");
+
+    }
+
+//    private static double getPriceOfJavaBooksPublicizedIn2002(List<Book> books){
+//        return 0;
+//    }
+
+//    private static void showNetBooksPublicizedIn2001(){
+//
+//    }
+}
